43fd55a921c0c9639a5e594aabfcfdf819eda863
vkganesan666 <vkganesan666@gmail.com>
2021-12-15 20:56:41 -0800

    added basic hello world screen

---
+++ front-end/screens/LogoScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const LogoScreen = () => {
+    return(
+    <View>
+        Hello World!
+    </View>
+    );
+}
+
+export default LogoScreen;
