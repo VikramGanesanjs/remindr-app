36b5dba69d1891d393e2f54259ba4c1beeb9dbcf
vkganesan666 <vkganesan666@gmail.com>
2021-12-16 21:48:20 -0800

    Started implementing UI and Firebase Authenticatio

--- front-end/screens/LogoScreen.tsx
+++ front-end/screens/LogoScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
 const LogoScreen = () => {
     return(
-    <View>
-        Hello World!
+    <View style={tw`flex justify-center items-center bg-blue-100 pt-20 items-stretch`}>
+        <Text style={tw`items-center`}>
+            Hello World!
+        </Text>
+        
     </View>
     );
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: [],
   theme: {
+    colors:{
+      gray: '#36454F',
+      purple: '#ACACE6',
+      green: '#0DBA86',
+      blue: '#ADD8E6',
+      red: '#FF5349'
+    },
     extend: {},
   },
   plugins: [],
